efadfcc97e6fe2c568f3bec9beff845b11d5f574
Alfred01-HUB <alfredolabanji066@gmail.com>
2022-12-08 12:44:22 -0800

    display prompt

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,14 @@
-#!/bin/bash
-gcc puts("programming is like building a multilingual puzzle")
+#include <stdio.h>
+
+
+/**
+ * main - Entry Point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+
+}
