cf09fa399fdf3f20029ac36dd118d3b25f42d448
Alfred01-HUB <alfredolabanji066@gmail.com>
2022-12-11 12:20:18 -0800

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,13 +3,14 @@
  * main - Entry point
  *
  * Return: Always 0 (success)
- */ 
+ */
 int main(void)
 {
 	char c;
 
 	c = 'a';
-	while (c <= 'z'){
+	while (c <= 'z')
+	 {
 		putchar(c);
 		c++;
 	}
