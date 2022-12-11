25c146d3cebf66ebafa8f3948aa4d11ef9e4a5b1
Alfred01-HUB <alfredolabanji066@gmail.com>
2022-12-11 12:33:52 -0800

    numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Entry point 
+ * main - Entry point
  *
  * Return: Always (0) (Success)
  */
@@ -12,7 +12,7 @@ int main(void)
 
 	while
 		(i <= 9) {
-			printf("%d", i);
+			printf ("%d", i);
 			i++;
 		}
 
