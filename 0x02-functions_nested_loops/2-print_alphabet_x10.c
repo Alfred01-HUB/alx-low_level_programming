4e39b19a59a01944621a92cb94eb027e5931855d
Alfred01-HUB <alfredolabanji066@gmail.com>
2022-12-13 07:44:49 -0800

    third push

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet - check the code
+ *
+ * Return: 0.
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	int j;
+
+	for (j = 1; j <= 10; j++)
+	{
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	}
+}
+
+
