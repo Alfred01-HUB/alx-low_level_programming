94a6f58b7ae501c344f2b742c6374fb71b747aad
Alfred01-HUB <alfredolabanji066@gmail.com>
2022-12-13 07:10:29 -0800

    lower alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - print alphabets
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
