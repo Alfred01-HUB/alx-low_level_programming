c1d799aaf153ea413c5936103170dc3383c0e570
Alfred01-HUB <alfredolabanji066@gmail.com>
2022-12-15 10:02:05 -0800

    task 1

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isupper - print which is upper or lower case
+ * @c:param
+ * Return: 0 and 1
+ */
+int _isupper(int c)
+{
+	int a;
+
+	for (a = 97; a <= 122; a++)
+	{
+		if ((c >= a) && (c >= a))
+		return (0);
+		else
+		return (1);
+	}
+}
+
