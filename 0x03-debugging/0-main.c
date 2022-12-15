8ca74663c6c97e41c7c48d64a4a579fecdd36144
Alfred01-HUB <alfredolabanji066@gmail.com>
2022-12-15 05:40:29 -0800

    task 1

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* positive_or_negative - prints if integer is positive or negative
+* @i:positive_negative integer
+* Return: 0
+*/
+void positive_or_negative(int i)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
+}
