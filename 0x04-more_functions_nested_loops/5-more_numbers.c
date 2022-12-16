7ec263ddd5d88ea971f8946bbf56573b0650f47f
Alfred01-HUB <alfredolabanji066@gmail.com>
2022-12-15 16:53:59 -0800

    pre morning

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * more_numbers - ten times a number
+ * Return: 0
+ */
+void more_numbers(void)
+{
+	int i , j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			_putchar(j + '0');
+		}
+		_putchar('\n');
+	}
+}
