da6cfdd4c77784e48b92aaeb9b68d50b9821c501
Alfred01-HUB <alfredolabanji066@gmail.com>
2022-12-20 14:58:05 -0800

    task4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,16 +1,22 @@
 #include "main.h"
+#include <string.h>
 /**
  * rev_string - print string in reverse order
  * @s: Strings to be printed in reverse order
  * Return: 0
  */
- void print_rev(char *s)
+void print_rev(char *s)
 {
-	char c;
-	c = *s;
-	for (; 'c' == '\0'; c--)
+	char *c = s;
+
+	int i;
+
+	int j = strlen(s);
+
+
+	for (i = j; i >= 0; i--)
 	{
-		_putchar(c);
+		_putchar(c[i]);
 	}
 	_putchar('\n');
 }
