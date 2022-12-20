d3740b80d15b8e45c7ed5a610bd7564fc06825d5
Alfred01-HUB <alfredolabanji066@gmail.com>
2022-12-20 15:34:44 -0800

    task 6

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+ * rev_string - reversed strings
+ * @s:strings to be reversed
+ * Return: 0
+ */
+void rev_string(char *s)
+{
+	
+}
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * puts2 - print single character of a string
+ * @str: strings of characters to be printed
+ * Return: 0
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	int j;
+
+	while 
+		(str [i] != '\0')
+		{
+			i++;
+		}
+	for (j = i; j < i; j += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
