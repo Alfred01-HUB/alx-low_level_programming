0b67ba74bfa672a795c2407d24b58cb6b4ff71a2
Alfred01-HUB <alfredolabanji066@gmail.com>
2022-12-20 07:58:49 -0800

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - print string lenght
+ * @s:param
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	int c = 0;
+
+		while 
+			(*s != '\0')
+			{
+				c++;
+			}
+		return (c);
+}
+				
+
