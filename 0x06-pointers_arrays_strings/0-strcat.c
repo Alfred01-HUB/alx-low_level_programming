31515469fc3f32b217e28f93744c9dd79618a73d
Alfred01-HUB <alfredolabanji066@gmail.com>
2022-12-29 07:03:36 -0800

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,6 +2,8 @@
 #include <string.h>
 /**
  * _strcat - concatenate two strings
+ * @dest:character to be concatenated
+ * @src:character to be concatenated
  * Return: 0
  */
 char *_strcat(char *dest, char *src)
